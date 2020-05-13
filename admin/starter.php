9edaa840343298b5a7783d699f00c673cb9c66ed
Sumeet <25149022+sumeetweb@users.noreply.github.com>
2020-05-13 22:50:26 +0530

    adding Admin scratch
    Using SB Admin 2

---
+++ admin/starter.php
@@ -0,0 +1,26 @@
+<?php
+
+require('include/header.php');
+
+
+
+echo <<<STRING
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <h1 class="h3 mb-4 text-gray-800">
+			Blank Page
+		  </h1>
+
+        </div>
+        <!-- /.container-fluid -->
+
+      </div>
+      <!-- End of Main Content -->
+STRING;
+
+
+require('include/footer.php');
+
+?>
